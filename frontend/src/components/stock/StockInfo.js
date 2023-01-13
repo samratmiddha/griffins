a72128019e759755496e5b26ca99128f018f859f
samratmiddha <s_middha@me.iitr.ac.in>
2023-01-13 23:28:58 +0530

    data extraction

--- frontend/src/components/stock/StockInfo.js
+++ frontend/src/components/stock/StockInfo.js
@@ -2,23 +2,40 @@ import { Box, Typography } from "@mui/material";
 import { useSearchParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { nanoid } from "nanoid";
 
 export default function StockInfo() {
   const [searchParams, setSearchParams] = useSearchParams();
   const symbol = searchParams.get("symbol");
   const [stockInfo, setStockInfo] = useState();
-  const [chartOption, setChartOption] = useState("1W");
+  const [chartOption, setChartOption] = useState("1Y");
+  const [stockDataset, setStockDataset] = useState([]);
   function handleCsv(data, instances) {
     var allRows = data.split(/\r?\n|\r/);
-    var table = "<table>";
-    for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
-      var rowCells = allRows[singleRow].split(",");
+    if (!instances) {
+      instances = allRows.length();
     }
+    var stockData = [];
+    for (var singleRow = 0; singleRow <= instances; singleRow++) {
+      var rowCells = allRows[singleRow].split(",");
+      console.log(rowCells);
+      stockData.push({ time: rowCells[0], price: rowCells[4] });
+    }
+    return stockData;
+  }
+  function getWeeklyData(data, instances) {
+    let s_data = [];
+    let keys = Object.keys(data);
+    instances = Math.min(keys.length, instances);
+    for (var row = 1; row <= instances; row++) {
+      s_data.push({ time: keys[row], price: data[keys[row]][4] });
+    }
+    return s_data;
   }
   useEffect(() => {
     axios
       .get(
-        `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${symbol}&apikey=SMP8GAVC4EB65JI0`
+        `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${symbol}&apikey=${nanoid()}`
       )
       .then((res) => {
         console.log("response", res);
@@ -31,10 +48,41 @@ export default function StockInfo() {
     if (chartOption === "1W") {
       axios
         .get(
-          "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=15min&slice=year1month1&apikey=SMP8GAVC4EB65JI0"
+          `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=15min&slice=year1month1&apikey=${nanoid}`
         )
         .then((res) => {
           console.log("intraday-data", res.data);
+          setStockDataset(handleCsv(res.data, 10));
+        });
+    } else if (chartOption === "1M") {
+      axios
+        .get(
+          `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=15min&slice=year1month1&apikey=${nanoid}`
+        )
+        .then((res) => {
+          setStockDataset(handleCsv(res.data));
+        });
+    } else if (chartOption === "3M") {
+      axios
+        .get(
+          `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&outputsize=compact&apikey=${nanoid()}`
+        )
+        .then((res) => {});
+    } else if (chartOption === "1Y") {
+      axios
+        .get(
+          `https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=${nanoid()}`
+        )
+        .then((res) => {
+          setStockDataset(getWeeklyData(res.data["Weekly Time Series"], 52));
+        });
+    } else if (chartOption === "5Y") {
+      axios
+        .get(
+          `https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=${nanoid()}`
+        )
+        .then((res) => {
+          setStockDataset(getWeeklyData(res.data["Weekly Time Series"], 260));
         });
     }
   }, [chartOption]);
