1477b5b5ba25b19567b7e8913df010b5a036b106
samratmiddha <s_middha@me.iitr.ac.in>
2023-01-13 14:06:18 +0530

    frontend folder and file structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "@emotion/react";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Stock from "./pages/Stock";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/stock" element={<Stock />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
