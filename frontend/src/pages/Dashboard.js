7850b41790c24035e0c9dd112d71a9f7fbaa867d
samratmiddha <s_middha@me.iitr.ac.in>
2023-01-14 06:26:12 +0530

    initialized dashboard styling

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,12 +1,26 @@
+import { Box, Typography } from "@mui/material";
 import SearchBox from "../components/dashboard/SearchBox";
 import TradeBar from "../components/stock/TradeBar";
 
 export default function Dashboard() {
   return (
-    <>
-      <h1>Dashboard</h1>
-      <TradeBar />
+    <Box
+      sx={{
+        backgroundColor: "#141518",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignContent: "center",
+      }}
+    >
+      <Typography
+        variant="h1"
+        sx={{ color: "#F3EF52", marginBottom: "2rem" }}
+        align="center"
+      >
+        Webster
+      </Typography>
       <SearchBox />
-    </>
+    </Box>
   );
 }
