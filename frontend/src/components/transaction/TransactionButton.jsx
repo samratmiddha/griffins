d21713e6c5e4733edf8ae0310665763df5c94ea6
samratmiddha <s_middha@me.iitr.ac.in>
2023-01-15 04:29:04 +0530

    resolving conflicts

--- frontend/src/components/transaction/TransactionButton.jsx
+++ frontend/src/components/transaction/TransactionButton.jsx
@@ -5,24 +5,14 @@ import axios from "axios";
 import React from "react";
 import { useSelector } from "react-redux";
 import emitSuccessToast from "../../utilities/emitSuccessToast";
-import emitWarnToast from "../../utilities/emitWarnToast";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import validate from "../../ipfs/validate";
-import checkBuffer from "../../ipfs/checkBuffer";
 
 function TransactionButton(props) {
-  let {selectedAmount,symbol,selectedUnits}=props;
-  
-  const currentUserAddress = useSelector((state) => state.user.userAddress);
-  let value = 0.001;
-  let symbol = "";
-  let quantity = "";
+  let { selectedAmount, symbol, selectedUnits } = props;
 
-  const ethToWeiHex = (eth) => {
-    const wei = eth * 10 ** 18;
-    return wei.toString(16);
-  };
+  const currentUserAddress = useSelector((state) => state.user.userAddress);
 
   const sendTransaction = async () => {
     if (!currentUserAddress) {
@@ -51,17 +41,28 @@ function TransactionButton(props) {
     // if (transaction) {
     //   emitSuccessToast("Transaction initiated.");
     // }
-    const contractAddr="0x6A2C6c42984f4265C569ba13aB062FCEc3f1ec57";
-    const provider=new ethers.providers.Web3Provider(window.ethereum);
+    const contractAddr = "0x6A2C6c42984f4265C569ba13aB062FCEc3f1ec57";
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
-    const contract=new ethers.Contract(contractAddr,oracleContractABI,signer);
+    const contract = new ethers.Contract(
+      contractAddr,
+      oracleContractABI,
+      signer
+    );
     // contract.validateTransaction(selectedAmount,symbol,selectedUnits).send({from:currentUserAddress, value: selectedAmount})
-    let weiAmount = String(Number(selectedAmount)*(10 ** 18))
-    contract.validateTransaction(symbol,selectedUnits,{value:weiAmount})
-    contract.on("StatusEvent",(status)=>{
-      console.log(status);
-    })
-   /* 
+    let weiAmount = String(Number(selectedAmount) * 10 ** 18);
+
+    let isValid = validate(currentUserAddress);
+    if (isValid) {
+      contract.validateTransaction(symbol, selectedUnits, { value: weiAmount });
+      contract.on("StatusEvent", (status) => {
+        console.log(status);
+      });
+    } else {
+      console.log("some error");
+    }
+
+    /* 
    console.log(currentUserAddress);
     const contractAddr = "0x23c1dFbbEBf49732f4C2A5b9E494062c1ff918de";
     const provider = new ethers.providers.Web3Provider(window.ethereum);
